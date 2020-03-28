28536a3037ba67dd9969c6c1b442369788065bf0
Vladislav Kalinin <unkorunk@gmail.com>
2020-03-29 03:56:09 +1000

    Transform WhileNode from do ... while(expr) to while(expr) ...

--- src/tree/WhileNode.kt
+++ src/tree/WhileNode.kt
@@ -6,8 +6,9 @@ import utility.Leb128
 class WhileNode(private val condition: ExprNode, private val thenNode: Node, nextNode: Node?) : Node(nextNode) {
     override fun generateCode(): ByteArray {
         return byteArrayOf(0x03).plus(Leb128.toSignedLeb128(-0x40))
+            .plus(condition.generateCode()).plus(0x45).plus(0x0d).plus(Leb128.toUnsignedLeb128(1))
             .plus(thenNode.getCode())
-            .plus(condition.generateCode()).plus(0x0d).plus(Leb128.toUnsignedLeb128(0))
+            .plus(0x0c).plus(Leb128.toUnsignedLeb128(0))
             .plus(0x0b)
     }
 }
