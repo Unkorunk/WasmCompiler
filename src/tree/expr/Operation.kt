94f1d3915508d8104eb9cfeb7cb27b62522c70f1
Vladislav Kalinin <unkorunk@gmail.com>
2020-03-29 18:25:08 +1000

    Fix bug: change Less bytecode from 0x4c(i32.le_s) to 0x48(i32.lt_s). i32.le_s - signed less than or equal. lt_s - signed less than.

--- src/tree/expr/Operation.kt
+++ src/tree/expr/Operation.kt
@@ -6,7 +6,7 @@ enum class Operation(val bytecode: Byte, val priority: Byte) {
     Mul(0x6c, 3),       // i32.mul
     Div(0x6d, 3),       // i32.div_s
     Greater(0x4a, 1),   // i32.gt_s
-    Less(0x4c, 1),      // i32.le_s
+    Less(0x48, 1),      // i32.lt_s
     Equal(0x46, 0),     // i32.eq
     UnEqual(0x47, 0)    // i32.ne
 }
